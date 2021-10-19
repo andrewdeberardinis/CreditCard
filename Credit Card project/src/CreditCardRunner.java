0eec5088c03e49cb47ab2233d6975e02dd2065db
andrew.deberardinis <andrew.deberardinis@MU-C1-06.mullen.pvt.k12.co.us>
2021-10-19 12:01:04 -0600

    first save for credit card

---
+++ Credit Card project/src/CreditCardRunner.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class CreditCardRunner
+	{
+	
+		
+			Scanner Searcher = new Scanner(new File("CreditCardFile.txt"));
+				
+			long number = 0;
+				
+
+		}	
+			
+			while(Searcher.hasNext())
+					{
+					String sentence = Searcher.nextLine();
+					System.out.println(sentence)
+					
+				
+					}
+
+		
+
+
---
+++ Credit Card project/src/LuhnChecker.java
@@ -0,0 +1,22 @@
+
+public class LuhnChecker
+	{
+
+		public static void main(String[] args)
+			{
+				
+				
+			
+				
+
+			}
+
+		long number [] = {5,4,2,4,1,8,0,1,2,3,4,5,6,7,8,9};
+		
+		
+		
+		
+	
+	
+	
+	}
